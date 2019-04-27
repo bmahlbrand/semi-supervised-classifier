734ce9b979003cbc1fcba2d52d0b7e22b8f5dfb8
Ben Ahlbrand <bmahlbrand@gmail.com>
2019-04-27 15:47:19 -0400

    TODO: implement plotting of history from config files

---
+++ plot_experiment.py
@@ -0,0 +1,4 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='PyTorch script for plotting results of experiments')
+parser.add_argument('--config', type=str, default='', help='config json file to reload experiments')
