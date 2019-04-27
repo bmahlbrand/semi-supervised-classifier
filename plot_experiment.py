7433e3e4e41244669d50c1ff9eb1683ee125577a
Ben Ahlbrand <bmahlbrand@gmail.com>
2019-04-27 16:22:23 -0400

    add import, finish later

--- plot_experiment.py
+++ plot_experiment.py
@@ -1,4 +1,6 @@
 import argparse
 
+from utils import viz_utils
+
 parser = argparse.ArgumentParser(description='PyTorch script for plotting results of experiments')
 parser.add_argument('--config', type=str, default='', help='config json file to reload experiments')
