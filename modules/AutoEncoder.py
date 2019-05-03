e8d7b939573f006517cdd5c1d7e57aaf2fec1ffa
Ben Ahlbrand <bmahlbrand@gmail.com>
2019-05-02 22:51:18 -0400

    fix autoencoder

--- modules/AutoEncoder.py
+++ modules/AutoEncoder.py
@@ -3,14 +3,15 @@ import torch.nn as nn
 from modules.ConvBlock import ConvBlock
 from modules.DeConvBlock import DeConvBlock
 import numpy as np
+
 class AutoEncoder(nn.Module):
     def __init__(self, num_classes=1000):
         super(AutoEncoder, self).__init__()
 
         # self.encoder = nn.Sequential(
-            
+        self.conv0 = nn.Conv2d(3, 64, kernel_size=1)
+
         self.encoder = {
-            'conv0': nn.Conv2d(3, 64, kernel_size=1),
             # (64)3c-4p-
             'conv1': ConvBlock(64, 64),
             'maxpool1': nn.MaxPool2d(kernel_size=4, return_indices=True),
@@ -31,7 +32,7 @@ class AutoEncoder(nn.Module):
             # (512)3c-
             'conv8': ConvBlock(512, 512),
             # (512)3c-
-            'conv9': ConvBlock(512, 10),
+            # 'conv9': ConvBlock(512, 10),
             'maxpool4': nn.MaxPool2d(kernel_size=2, return_indices=True),
             # (512)3c-2p-10fc 
             'fc1': nn.Linear(10, num_classes)
@@ -43,23 +44,24 @@ class AutoEncoder(nn.Module):
             # (512)3c-2p-10fc 
             'fc1': nn.Linear(num_classes, 10),
             # (512)3c-
-            'deconv1': DeConvBlock(10, 512),
+            'deconv1': DeConvBlock(512, 512),
             'unpool1': nn.MaxUnpool2d(kernel_size=2),
             # (512)3c-
             'deconv2': DeConvBlock(512, 512),
             # (256)3c-
             'deconv3': DeConvBlock(512, 512),
             # (256)3c-
-            'deconv4': DeConvBlock(256, 512),
+            'deconv4': DeConvBlock(512, 256),
             # (256)3c-
             'deconv5': DeConvBlock(256, 256),
             # (128)3c-2p-
-            'deconv6': DeConvBlock(128, 256),
             'unpool2': nn.MaxUnpool2d(kernel_size=2),
+            'deconv6': DeConvBlock(256, 128),
+            
             # (128)3c-
             'deconv7': DeConvBlock(128, 128),
             # (64)3c-3p-
-            'deconv8': DeConvBlock(64, 128),
+            'deconv8': DeConvBlock(128, 64),
             'unpool3': nn.MaxUnpool2d(kernel_size=3),
             # (64)3c-4p-
             'deconv9': DeConvBlock(64, 64),
@@ -68,7 +70,7 @@ class AutoEncoder(nn.Module):
 
         self.avgpool = nn.AdaptiveAvgPool2d(1)
         self.classifier = nn.Sequential(
-            nn.Linear(1000, 512),
+            nn.Linear(64, 512),
             nn.ReLU(True),
             nn.Dropout(),
             nn.Linear(512, 512),
@@ -79,7 +81,7 @@ class AutoEncoder(nn.Module):
 
     def forward(self, x):
         # print(x.shape)
-        x = self.encoder['conv0'](x)
+        x = self.conv0(x)
         # print(x.shape)
         x = self.encoder['conv1'](x)
         x, indices1 = self.encoder['maxpool1'](x)
@@ -92,36 +94,43 @@ class AutoEncoder(nn.Module):
         x = self.encoder['conv6'](x)
         x = self.encoder['conv7'](x)
         x = self.encoder['conv8'](x)
-        x = self.encoder['conv9'](x)
+        # x = self.encoder['conv9'](x)
         x, indices4 = self.encoder['maxpool4'](x)
+
+        # print(indices1.shape)
+        # print(indices2.shape)
+        # print(indices3.shape)
+        # print(indices4.shape)
         # print(x.shape)
+        # x = self.avgpool(x
+        # x = x.view(x.size(0), -1)
+        # # x = x.view(-1, np.prod(x.shape))
+        
+        
+        # x = self.encoder['fc1'](x)
+
+        # x = self.decoder['fc1'](x)
+        
+        x = self.decoder['deconv1'](x)
+        x = self.decoder['unpool1'](x, indices4)
+        x = self.decoder['deconv2'](x)
+        x = self.decoder['deconv3'](x)
+        x = self.decoder['deconv4'](x)
+        x = self.decoder['deconv5'](x)
+        x = self.decoder['unpool2'](x, indices3)
+        x = self.decoder['deconv6'](x)
+        
+        x = self.decoder['deconv7'](x)
+        x = self.decoder['unpool3'](x, indices2)
+        x = self.decoder['deconv8'](x)
+        x = self.decoder['unpool4'](x, indices1)
+        x = self.decoder['deconv9'](x)
+        
+        # x = self.decoder['fc1'](x)
+
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        # x = x.view(-1, np.prod(x.shape))
-        
-        
-        x = self.encoder['fc1'](x)
-
-        # x = self.decoder['fc1'](x)
-        
-        # x = self.decoder['deconv1'](x)
-        # x = self.decoder['unpool1'](x, indices1)
-        # x = self.decoder['deconv2'](x)
-        # x = self.decoder['unpool2'](x, indices2)
-        # x = self.decoder['deconv3'](x)
-        # x = self.decoder['deconv4'](x)
-        # x = self.decoder['unpool3'](x, indices3)
-        # x = self.decoder['deconv5'](x)
-        # x = self.decoder['deconv6'](x)
-        # x = self.decoder['deconv7'](x)
-        # x = self.decoder['deconv8'](x)
-        # x = self.decoder['deconv9'](x)
-        # x = self.decoder['unpool4'](x, indices4)
-        # x = self.decoder['fc1'](x)
-
         x = self.classifier(x)
 
-        # x = self.avgpool(x)
-        # x = x.view(x.size(0), -1)
 
         return x
--- modules/DeConvBlock.py
+++ modules/DeConvBlock.py
@@ -4,10 +4,10 @@ class DeConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DeConvBlock, self).__init__()
 
-        self.conv = nn.Sequential(
+        self.deconv = nn.Sequential(
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True))
     
     def forward(self, x):
-        return self.conv(x)
+        return self.deconv(x)
